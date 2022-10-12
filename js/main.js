8fefbf56048d8bbe24fd981155062795288448e8
Eran Peled <peled2k@gmail.com>
2022-10-12 20:59:42 +0300

    step 1 + 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,77 @@
 'use strict'
 console.log('Starting up');
 
-function createProj(){
-    
+renderProjectModal()
+renderPortfolio()
+
+function renderProjectModal(){
+    var projs = gProjs
+    const strHtmls = projs.map(proj => {
+        return `<div class="portfolio-modal modal fade" id="${proj.id}" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="close-modal" data-dismiss="modal">
+                        <div class="lr">
+                            <div class="rl"></div>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-8 mx-auto">
+                                <div class="modal-body">
+                                    <!-- Project Details Go Here -->
+                                    <h2>${proj.name}</h2>
+                                    <p class="item-intro text-muted">${proj.title}</p>
+                                    <img class="img-fluid d-block mx-auto" src= ${proj.img} alt="">
+                                    <p>${proj.desc}</p>
+                                    <ul class="list-inline">
+                                        <li>Date: ${proj.publishedAt}</li>
+                                        <li>Client: <a href= ${proj.url}>go to preject </a></li>
+                                        <li>Category: ${proj.labels[0]}</li>
+                                    </ul>
+                                    <button class="btn btn-primary" data-dismiss="modal" type="button">
+                                        <i class="fa fa-times"></i>
+                                        Close Project</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    }).join('')
+    document.getElementById('portfolioModal').innerHTML = strHtmls
 }
+
+function renderPortfolio(){
+    var projs = gProjs
+    document.getElementById('portfolio').innerHTML = 
+    `<div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Portfolio</h2>
+                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                </div>
+            </div>`
+    const strHtmls = projs.map(proj => {
+        return `
+            <div class="col-md-4 col-sm-6 portfolio-item">
+                <a class="portfolio-link" data-toggle="modal" href="#${proj.id}">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content">
+                            <i class="fa fa-plus fa-3x"></i>
+                        </div>
+                    </div>
+                    <img class="img-fluid" src="${proj.img}" alt="">
+                </a>
+                <div class="portfolio-caption">
+                    <h4>${proj.name}</h4>
+                    <p class="text-muted">${proj.title}</p>
+                </div>
+            </div>`
+    }).join('')
+    document.getElementById('portfolio').innerHTML += '<div class="row"> ' + strHtmls + '</div> </div>'
+}
+
+
+/*  */
--- js/services/proj-service.js
+++ js/services/proj-service.js
@@ -1,63 +1,54 @@
 'use strict'
 const STORAGE_KEY = 'projDB'
 
-var gProj
+var gProjs
 var gFilterBy
 var gOpenBook
 var gBookId
 
-_createbooks()
+_createprojs()
 
-function _createbook(name,title,url,publishedAt,) {
-    return {
-        id: makeId(), 
-        name, 
-        title, 
-        desc: "lorem ipsum lorem ipsum lorem ipsum", 
-        url, 
-        publishedAt, 
-        labels: ["Matrixes", "keyboard events"],
-    }
-}
-
-function _createbooks() {
+function _createprojs() {
     var projs = loadFromStorage(STORAGE_KEY)
     // Nothing in storage - generate demo data
     if (!projs || !projs.length) {
         projs = [
             {
-                id: "mine-sweeper", 
+                id: 'mine-sweeper', 
                 name: 'MineSweeper', 
                 title: 'Find all the mines', 
-                desc: "lorem ipsum lorem ipsum lorem ipsum", 
-                url, 
-                publishedAt, 
-                labels: ["Matrixes", "keyboard events"],
+                desc: "lorem ipsum lorem ipsum lorem ipsum",
+                img: 'img/portfolio/minesweeper.png', 
+                url: 'projects/mine-sweeper/index.html', 
+                publishedAt: '29/09/2022', 
+                labels: ["Matrixes", "mouse events"],
             },
             {
-                id: 'to-do', 
+                id: 'mvc-to-do', 
                 name: 'MvcToDo', 
-                title, 
-                desc: "lorem ipsum lorem ipsum lorem ipsum", 
-                url, 
-                publishedAt, 
-                labels: ["Matrixes", "keyboard events"],
+                title: 'You can manage all of your tasks', 
+                desc: "lorem ipsum lorem ipsum lorem ipsum",
+                img: 'img/portfolio/to-do-list.png', 
+                url: 'projects/mvc-to-do/index.html', 
+                publishedAt: '03/10/2022', 
+                labels: ["MVC", "mouse events"],
             },
             {
                 id: 'book-shop', 
                 name: 'BookShop', 
-                title, 
-                desc: "lorem ipsum lorem ipsum lorem ipsum", 
-                url, 
-                publishedAt, 
-                labels: ["Matrixes", "keyboard events"],
+                title: 'Check the price rate and summery of the latest books', 
+                desc: "lorem ipsum lorem ipsum lorem ipsum",
+                img: 'img/portfolio/bookshop.jpg', 
+                url: 'projects/book-shop/index.html', 
+                publishedAt:'06/10/2022', 
+                labels: ["CRUDL", "mouse events"],
             },
         ]
     }
-    gProj = projs 
-    _saveBooksToStorage()
+    gProjs = projs 
+    _saveprojsToStorage()
 }
 
-function _saveBooksToStorage(key = STORAGE_KEY, val = gProj){
+function _saveprojsToStorage(key = STORAGE_KEY, val = gProjs){
     saveToStorage(key, val)
 }
